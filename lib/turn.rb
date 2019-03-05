6b94f1b230e1e6bfe8ddf8c2f3adad6f77ac7fe7
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-05 22:05:24 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+require 'pry'
+def display_board(board)
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts " -----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input= user_input.to_i 
+  user_input -= 1 
+end
+
+def valid_move?(board, index)
+if index.between?(0,8) && board[index] != "X" && board[index] != "O"
+  true
+else
+  false 
+end 
+end
+
+def move(board, index, token)
+  board[index]= token
+end 
+
+def turn(board)
+  puts "Please enter 1-9:"
+  number = gets 
+  index=input_to_index(number) 
+  binding.pry
+  if valid_move?(board, index)
+    true
+  else 
+    turn(board)
+  end
+end 
